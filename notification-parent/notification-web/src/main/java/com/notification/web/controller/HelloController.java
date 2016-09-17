1ac5b04f140f99facf92cc197a713acfd6874717
Srivastava <464351@CTS.COM>
2016-09-17 13:35:15 +0530

    Code changes for geting status

--- notification-parent/notification-web/src/main/java/com/notification/web/controller/HelloController.java
+++ notification-parent/notification-web/src/main/java/com/notification/web/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.notification.web.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,19 @@ import com.notifications.service.HelloService;
 public class HelloController {
 	@Autowired
 	private HelloService helloService;
+	
+	
 	@RequestMapping(value="/userDetails/{userName}",method = RequestMethod.GET)
 	@ResponseBody
 	public void isUserName (@PathVariable String userName){
 	System.out.println("==============>"+userName);
 	helloService.getName(userName);
+	} 
+	
+	@RequestMapping(value="/status",method = RequestMethod.GET)
+	@ResponseBody
+	public String getStatus (){
+	
+	return "Version 1.0 : "+ HttpStatus.OK.toString() + "  Ok";
 	} 
 }
