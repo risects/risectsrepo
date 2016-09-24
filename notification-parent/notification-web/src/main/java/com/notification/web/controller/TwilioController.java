2dd13ea106e12ac826bb3fc10eb0087045db4d45
Manish <Manish@Manish-PC>
2016-09-24 11:33:23 +0530

    otp api

--- notification-parent/notification-web/src/main/java/com/notification/web/controller/TwilioController.java
+++ notification-parent/notification-web/src/main/java/com/notification/web/controller/TwilioController.java
@@ -9,21 +9,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.twilio.sdk.TwilioRestException;
 
 @Controller
-public class TwilioController {
+public class TwilioController
+{
 
-
-		@RequestMapping(value="/sendSms/{toNumber}/{bodyMessage}",method = RequestMethod.POST)
-		@ResponseBody
-		public String sendSms (@PathVariable("toNumber") String toNumber, @PathVariable("bodyMessage") String bodyMessage){
+	@RequestMapping(value = "/sendSms/{toNumber}/{bodyMessage}", method = RequestMethod.POST)
+	@ResponseBody
+	public String sendSms(@PathVariable("toNumber") String toNumber, @PathVariable("bodyMessage") String bodyMessage)
+	{
 		String response = "Message sent to " + toNumber;
-		try {
-			new SmsSender().sendMessage(toNumber,bodyMessage);
-		} catch (TwilioRestException e) {
+		try
+		{
+			new SmsSender().sendMessage(toNumber, bodyMessage);
+		}
+		catch(TwilioRestException e)
+		{
 			e.printStackTrace();
 			response = "Unable to sent sms to " + toNumber;
 		}
 		return response;
-		} 
-	
+	}
+
+	@RequestMapping(value = "/otp/{emailId}/{flag}", method = RequestMethod.GET)
+	@ResponseBody
+	public String getOTP(@PathVariable("emailId") String emailId, @PathVariable("flag") String flag)
+	{
+		int randomPIN = (int) (Math.random() * 9000) + 1000;
+		StringBuilder otpBuilder = new StringBuilder(flag);
+		otpBuilder.append(randomPIN);
+		otpBuilder.append(flag);
+		return otpBuilder.toString();
+
+	}
 
 }
